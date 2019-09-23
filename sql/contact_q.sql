dcf5d7e5459b8b7834d08ff58a61059005bd310f
Miraj <mirajaryal@gmail.com>
2019-09-23 12:57:54 +1000

    sql file added

---
+++ sql/contact_q.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.7
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 23, 2019 at 02:57 AM
+-- Server version: 5.6.37
+-- PHP Version: 7.1.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cv_contact`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_q`
+--
+
+CREATE TABLE IF NOT EXISTS `contact_q` (
+  `fname` text NOT NULL,
+  `lname` text NOT NULL,
+  `email` varchar(500) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `institute` text NOT NULL,
+  `role` text NOT NULL,
+  `message` text NOT NULL,
+  `active` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contact_q`
+--
+
+INSERT INTO `contact_q` (`fname`, `lname`, `email`, `phone`, `institute`, `role`, `message`, `active`, `id`) VALUES
+('Miraj', 'Aryal', 'mirajaryal@gmail.com', '411381165', 'Fed', 'Gradd', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum', 1, 1),
+('Nirusha', 'Khadka', 'nirushakhadka977@gmail.com', '411381165', 'AIa', 'Chidlcare educator', 'THis is a stest', 1, 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact_q`
+--
+ALTER TABLE `contact_q`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contact_q`
+--
+ALTER TABLE `contact_q`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
