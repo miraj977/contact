042784d97a8e36c396c943eb79a9f41230dd9c93
Miraj <mirajaryal@gmail.com>
2019-09-23 12:33:17 +1000

    Query details page added

---
+++ details.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require 'functions/connect.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Query Details</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/style.css"> 
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    </head>
+    <body>
+        <?php ?>
+    </body>
+</html>
+
